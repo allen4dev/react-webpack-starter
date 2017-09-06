260fb398c173b947b14b7b9415e48a0668987f18
allen4dev <alanaliagadev@gmail.com>
2017-09-06 12:09:46 -0400

    [Add] webpack-merge

--- webpack/webpack.client.config.js
+++ webpack/webpack.client.config.js
@@ -1,57 +1,35 @@
 const { resolve } = require('path');
-const webpack = require('webpack');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
 
-const config = {
-  context: resolve(__dirname, '..'),
-  entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    resolve('src', 'index.js'),
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: resolve('dist'),
+const commonConfig = require('./webpack.common');
+const parts = require('./webpack.parts');
+
+const config = merge([
+  // loaders
+  parts.injectCSS(),
+  // plugins
+  parts.setupHTML({
+    title: 'Simple webpack starter',
+    template: 'index.ejs',
+  }),
+  parts.setupHMR(),
+  // config
+  parts.devServer(),
+  {
+    context: resolve(__dirname, '..'),
+    entry: [resolve('src', 'index.js')],
+    output: {
+      filename: 'bundle.js',
+      path: resolve('dist'),
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', 'json'],
+    },
+    target: 'web',
   },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css?$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 20000,
-          },
-        },
-      },
-    ],
-  },
-  devServer: {
-    hot: true,
-    // publicPath: '/dist',
-  },
-  plugins: [
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new HTMLWebpackPlugin({
-      title: 'Simple webpack starter',
-      template: 'index.ejs',
-    }),
-  ],
-  resolve: {
-    extensions: ['.js', '.jsx', 'json'],
-  },
-  target: 'web',
-};
+  commonConfig,
+]);
+
+console.log(config);
 
 module.exports = config;
---
+++ webpack/webpack.parts.js
@@ -0,0 +1,86 @@
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+// Loaders
+exports.babelTranspile = function babelTranspile() {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          use: 'babel-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+  };
+};
+
+exports.injectCSS = function injectCSS() {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.css?$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+  };
+};
+
+exports.loadImages = function loadImages({ options }) {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.(jpe?g|png|gif|svg)$/,
+          use: {
+            loader: 'url-loader',
+            options,
+            // options: {
+            //   limit: 20000,
+            // },
+          },
+        },
+      ],
+    },
+  };
+};
+// Plugins
+exports.setupHMR = function setupHMR() {
+  return {
+    entry: [
+      'react-hot-loader/patch',
+      'webpack-dev-server/client?http://localhost:8080',
+      'webpack/hot/only-dev-server',
+    ],
+    plugins: [
+      new webpack.NamedModulesPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+    ],
+  };
+};
+
+exports.setupHTML = function setupHTML({ title, template }) {
+  return {
+    plugins: [
+      new HTMLWebpackPlugin({
+        title,
+        template,
+        // title: 'Simple webpack starter',
+        // template: 'index.ejs',
+      }),
+    ],
+  };
+};
+
+// Config
+exports.devServer = function devServer() {
+  return {
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+    },
+  };
+};
